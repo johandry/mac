c8574afb6a038b7a830844b76303cfe1a0b4a5ee
Johandry Amador <johandry@gmail.com>
2015-08-23 22:45:08 -0700

    New Applications added
    VirtualBox, Vagrant, Packer, Docker ToolBox, Caffeine, Firefox,
    SourceTree, Colloquy, GitHub Desktop, HandBrake, HandBrake CLI, Skype,
    Spectacle

--- setup.sh
+++ setup.sh
@@ -87,7 +87,8 @@ install_app_store 'iMovie' '/Applications/iMovie.app/Contents/MacOS/iMovie' 'itu
 install_app_store 'Keynote' '/Applications/Keynote.app/Contents/MacOS/Keynote' 'itunes.apple.com/us/app/keynote/id409183694?mt=12'
 install_app_store 'Numbers' '/Applications/Numbers.app/Contents/MacOS/Numbers' 'itunes.apple.com/us/app/numbers/id409203825?mt=12'
 install_app_store 'Pages' '/Applications/Pages.app/Contents/MacOS/Pages' 'itunes.apple.com/us/app/pages/id409201541?mt=12'
-install_app_store 'SourceTree' '/Applications/SourceTree.app/Contents/MacOS/SourceTree' 'itunes.apple.com/us/app/sourcetree-git-hg/id411678673?mt=12'
+#install_app_store 'SourceTree' '/Applications/SourceTree.app/Contents/MacOS/SourceTree' 'itunes.apple.com/us/app/sourcetree-git-hg/id411678673?mt=12'
+#SourceTree is not longer up to date in App Store. Installing with BrewCask
 install_app_store 'HP Easy Scan' '/Applications/HP Easy Scan.app/Contents/MacOS/HP Easy Scan' 'itunes.apple.com/us/app/hp-easy-scan/id967004861?mt=12'
 install_app_store 'MindNode 2' '/Applications/MindNode.app/Contents/MacOS/MindNode' 'itunes.apple.com/us/app/mindnode-2-delightful-mind/id992076693?mt=12'
 install_app_store 'Twitter' '/Applications/Twitter.app/Contents/MacOS/Twitter' 'itunes.apple.com/us/app/twitter/id409789998?mt=12'
@@ -101,6 +102,20 @@ install 'Wget' '/usr/local/bin/wget' 'brew install wget'
 install 'Brew Cask' '/usr/local/bin/brew-cask' 'brew install caskroom/cask/brew-cask'
 install 'Dropbox' "$HOME/Applications/Dropbox.app/Contents/MacOS/Dropbox" 'brew-cask install dropbox'
 install 'Box Sync' "$HOME/Applications/Box Sync.app/Contents/MacOS/Box Sync" 'brew-cask install box-sync'
+install 'Virtual Box' "/Applications/VirtualBox.app/Contents/MacOS/virtualbox" 'brew-cask install virtualbox'
+install 'Vagrant' "/usr/local/bin/vagrant" 'brew-cask install vagrant'
+install 'Packer' "/usr/local/bin/packer" 'brew-cask install packer'
+install 'Docker Tool Box' "/Applications/Docker/Kitematic (Beta).app/Contents/MacOS/Electron" 'brew-cask install dockertoolbox'
+install 'Caffeine' "$HOME/Applications/Caffeine.app/Contents/MacOS/Caffeine" 'brew-cask install caffeine'
+install 'Firefox' "$HOME/Applications/Firefox.app/Contents/MacOS/firefox" 'brew-cask install firefox'
+install 'SourceTree' '/usr/local/bin/stree' 'brew-cask install sourcetree'
+install 'Colloquy' "$HOME/Applications/Colloquy.app/Contents/MacOS/Colloquy" 'brew-cask install colloquy'
+install 'GitHub' '/usr/local/bin/github' 'brew-cask install github-desktop'
+install 'HandBrake' "$HOME/Applications/HandBrake.app/Contents/MacOS/HandBrake" 'brew-cask install handbrake'
+install 'HandBrake CLI' '/usr/local/bin/HandBrakeCLI' 'brew-cask install handbrakecli'
+install 'Skype' "$HOME/Applications/Skype.app/Contents/MacOS/Skype" 'brew-cask install skype'
+install 'Spectacle' "$HOME/Applications/Spectacle.app/Contents/MacOS/Spectacle" 'brew-cask install spectacle'
+
 
 echo
 echo "Manual Installs"
