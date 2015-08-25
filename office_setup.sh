1d90e8792fe08d0509cb2a83c6a8f8a0facba9d9
Johandry Amador <JohandryAmador@fico.com>
2015-08-24 22:24:45 -0700

    Office setup updated

--- office_setup.sh
+++ office_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 declare -r SETUP_DIR="$( cd "$( dirname "$0" )" && pwd )"
-source "${SETUP_DIR}/common.sh"
+source "${SETUP_DIR}/files/common.sh"
 source "${SETUP_DIR}/functions.sh"
 
 echo
@@ -11,7 +11,7 @@ echo
 create_dir 'Workspace & Sandbox' "$HOME/Workspace/Sandbox"
 create_dir 'Local bin' "$HOME/bin"
 
-copy_file 'Common script' "${SETUP_DIR}/common.sh" "$HOME/bin/common.sh"
+copy_file 'Common script' "${SETUP_DIR}/files/common.sh" "$HOME/bin/common.sh"
 
 echo
 echo "App Store Installs"
@@ -25,6 +25,7 @@ install_app_store 'Numbers' 		'/Applications/Numbers.app/Contents/MacOS/Numbers'
 install_app_store 'Pages' 			'/Applications/Pages.app/Contents/MacOS/Pages' 'itunes.apple.com/us/app/pages/id409201541?mt=12'
 install_app_store 'MindNode 2' 	'/Applications/MindNode.app/Contents/MacOS/MindNode' 'itunes.apple.com/us/app/mindnode-2-delightful-mind/id992076693?mt=12'
 install_app_store 'Twitter' 		'/Applications/Twitter.app/Contents/MacOS/Twitter' 'itunes.apple.com/us/app/twitter/id409789998?mt=12'
+install_app_store 'OneDrive'    '/Applications/OneDrive.app/Contents/MacOS/OneDrive' 'itunes.apple.com/us/app/onedrive/id823766827?mt=12' 'echo "Configure OneDrive" && open /Applications/OneDrive.app'
 
 echo
 echo "Applications"
@@ -33,29 +34,38 @@ echo
 install 'Homebrew' 					'/usr/local/bin/brew' 'ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"'
 install 'Wget' 							'/usr/local/bin/wget' 'brew install wget'
 install 'Brew Cask' 				'/usr/local/bin/brew-cask' 'brew install caskroom/cask/brew-cask'
-install 'Dropbox' 					"$HOME/Applications/Dropbox.app/Contents/MacOS/Dropbox" 'brew-cask install dropbox'
-install 'Box Sync' 					"$HOME/Applications/Box Sync.app/Contents/MacOS/Box Sync" 'brew-cask install box-sync'
-install 'Virtual Box' 			"/Applications/VirtualBox.app/Contents/MacOS/virtualbox" 'brew-cask install virtualbox'
-install 'Vagrant' 					"/usr/local/bin/vagrant" 'brew-cask install vagrant'
-install 'Packer' 						"/usr/local/bin/packer" 'brew-cask install packer'
-install 'Docker Tool Box' 	"/Applications/Docker/Kitematic (Beta).app/Contents/MacOS/Electron" 'brew-cask install dockertoolbox'
-install 'Caffeine' 					"$HOME/Applications/Caffeine.app/Contents/MacOS/Caffeine" 'brew-cask install caffeine'
-install 'Firefox' 					"$HOME/Applications/Firefox.app/Contents/MacOS/firefox" 'brew-cask install firefox'
-install 'GitHub' 		        '/usr/local/bin/github' 'brew-cask install github'
-install 'Skype' 						"$HOME/Applications/Skype.app/Contents/MacOS/Skype" 'brew-cask install skype'
-install 'Spectacle' 				"$HOME/Applications/Spectacle.app/Contents/MacOS/Spectacle" 'brew-cask install spectacle'
-# Applications to install:
-# install 'Google Drive'
-# install 'Spotify'
-# install 'TorBrowser'
-# install 'Transmission'
-# install 'UnRarX'
-# install 'VLC'
-# install 'Microsoft Silverlight'
-# install 'NETGEARGenie'
-# install 'OneDrive'
+install 'Oh-My-Zsh'         "$HOME/.oh-my-zsh/oh-my-zsh.sh" 'curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh'
+
+create_dir 'Oh-My-Zsh Custom Themes Directory' "$HOME/.oh-my-zsh/custom/themes"
+copy_file 'Oh-My-Zsh Custom Theme ' "${SETUP_DIR}/files/af-magic-clean.zsh-theme" "$HOME/.oh-my-zsh/custom/themes/af-magic-clean.zsh-theme"
+
+# Cloud Storage
+install 'Dropbox'           "$HOME/Applications/Dropbox.app/Contents/MacOS/Dropbox" 'brew-cask install dropbox'
+install 'Box Sync'          "$HOME/Applications/Box Sync.app/Contents/MacOS/Box Sync" 'brew-cask install box-sync'        
+# DevOps Tools
+install 'Virtual Box'       "/Applications/VirtualBox.app/Contents/MacOS/virtualbox" 'brew-cask install virtualbox'
+install 'Vagrant'           "/usr/local/bin/vagrant" 'brew-cask install vagrant'
+install 'Packer'            "/usr/local/bin/packer" 'brew-cask install packer'
+install 'Docker Tool Box'   "/Applications/Docker/Kitematic (Beta).app/Contents/MacOS/Electron" 'brew-cask install dockertoolbox'
+install 'GitHub Desktop'    '/usr/local/bin/github' 'brew-cask install github-desktop'
+# Browsers
+install 'Firefox'           "$HOME/Applications/Firefox.app/Contents/MacOS/firefox" 'brew-cask install firefox'
+# Movies
+install 'VLC'               "$HOME/Applications/VLC.app/Contents/MacOS/VLC" 'brew-cask install vlc'
+# Music
+install 'Spotify'           "$HOME/Applications/Spotify.app/Contents/MacOS/Spotify" 'brew-cask install spotify'
+# Chat
+install 'Skype'             "$HOME/Applications/Skype.app/Contents/MacOS/Skype" 'brew-cask install skype'
+# File Management
+install 'Transmission'      "$HOME/Applications/Transmission.app/Contents/MacOS/Transmission" 'brew-cask install transmission'
+install 'UnRarX'            "$HOME/Applications/UnRarX.app/Contents/MacOS/UnRarX" 'brew-cask install unrarx'
+# Miscellaneous 
+install 'Caffeine'          "$HOME/Applications/Caffeine.app/Contents/MacOS/Caffeine" 'brew-cask install caffeine'
+install 'Spectacle'         "$HOME/Applications/Spectacle.app/Contents/MacOS/Spectacle" 'brew-cask install spectacle'
 
 # Applications to think about:
+# install 'Google Drive'
+# install 'Microsoft Silverlight'
 # install 'Visual Studio Code'
 # install 'iChm'
 # install 'Art Text 2'
