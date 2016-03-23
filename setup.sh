52aad3113a2850408eea8afb0abaa1831a3053aa
Johandry Amador <Johandry@gmail.com>
2016-03-23 12:00:36 -0700

    Adding editors Atom and Sublime Text 3

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 #=======================================================================================================
 # Author: Johandry Amador <johandry@gmail.com>
-# Title: My Mac provisioning 
+# Title: My Mac provisioning
 #
 # Usage: {script_name} [-h | configuration]
 #
@@ -10,7 +10,7 @@
 #     -h, --help    Display this help message. bash {script_name} -h
 #     configuration Configuration to load to provisioning this Mac. If not provided will use the hostname
 #
-# Description: This script is to provisioning or install a applications in a Mac. The applications to install are in configuration files. 
+# Description: This script is to provisioning or install a applications in a Mac. The applications to install are in configuration files.
 #              When there is a new application, it has to be added in config.sh then in the configuration file where it will be installed.
 #
 # Report Issues or create Pull Requests in http://github.com/johandry/
@@ -20,23 +20,23 @@
 declare -r SETUP_DIR="$( cd "$( dirname "$0" )" && pwd )"
 source "${SETUP_DIR}/files/common.sh"
 source "${SETUP_DIR}/functions.sh"
-source "${SETUP_DIR}/config.sh"  
+source "${SETUP_DIR}/config.sh"
 
 if [[ -z $1 ]]
   then
   if [[ -e "${SETUP_DIR}/$HOSTNAME.cnf" ]]
     then
     info "Loading $HOSTNAME.cnf configuration"
-    source "${SETUP_DIR}/$HOSTNAME.cnf" 
+    source "${SETUP_DIR}/$HOSTNAME.cnf"
   else
     error "The configuration file for this host ($HOSTNAME) was not found. Provide a configuration file to load."
     exit 1
   fi
 else
   if [[ -e "${SETUP_DIR}/$1.cnf"  ]]
-    then 
+    then
     info "Loading $1.cnf configuration"
-    source "${SETUP_DIR}/$1.cnf" 
+    source "${SETUP_DIR}/$1.cnf"
   else
     error "Configuration file for $1 was not found. Enter a valid configuration ($(ls ${SETUP_DIR}/*.cnf | sed 's/\(.*\)\.cnf/\1/' | tr '\n' ',' | sed 's/,/, /'))"
     exit 1
@@ -70,5 +70,4 @@ echo
 echo "Manual Installs"
 echo
 
-install_sublime_text_3
 install_google_chrome
