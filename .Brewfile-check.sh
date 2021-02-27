b8f2d8c9b9f7266c2e68823815762892162ed4ae
Johandry Amador <johandry@gmail.com>
2021-02-26 21:05:46 -0800

    Script to check installed CLI programs

---
+++ .Brewfile-check.sh
@@ -0,0 +1,152 @@
+
+# Mac App Store command-line interface
+mas version
+
+# Docker
+docker --version
+
+# Docker Compose
+docker-compose --version
+
+# Editor and IDE Visual Studio Code instead of Atom (cask "atom")
+code --version
+
+# Development Tools
+git --version
+ag --version | head -1
+colordiff --version | head -1
+
+# Bash Development
+bash --version | head -1
+bats --version
+netcat --version | head -1
+
+# API Development (gRPC and REST)
+grpcurl --version
+http --version
+
+# JSON & YAML
+jq --version
+yq --version
+
+# Local DB
+sqlite3 --version
+
+# Documentation
+asciinema --version
+
+# Groovy/Jenkins Development
+groovy --version
+
+# Go Development
+go version
+
+# External tools
+richgo version
+dot -V
+go-bindata --version | head -1
+
+# Rust Development
+rustc --version
+
+# External tools
+rustup-init --version
+
+# Python Development
+python --version
+python3 --version
+
+# External tools
+jupyter --version
+pyenv --version
+
+# Node Development
+node --version
+npm --version
+
+# External tools
+yarn --version
+
+# Infrastructure
+
+# Terraform
+terraform version
+
+# Machine Image Provisioner
+packer version
+
+# Other Hashicorp tools
+vault version
+serf version | head -1
+
+# Virtual Machines Engines
+VirtualBox --help | head -1
+
+# Vagrant
+vagrant version | head -1
+
+# Cloud Platforms
+
+# AWS
+aws --version
+aws-iam-authenticator version
+s3cmd --version
+
+# GCP
+gcloud --version
+
+# Azure
+az --version
+
+# IBM 
+ibmcloud --version
+
+# KubeCTL
+kubectl version --short --client
+
+# OpenShiftCTL
+oc version
+
+# Kubernetes clusters provisioners
+kind --version
+minikube version
+
+# Utils
+watch --version
+
+# Development tools
+kubebuilder version
+helm version
+
+# Utils
+tree --version
+magick --version | head -1
+base64 --version | head -1
+revolver --version
+fzf --version
+autojump --version
+grc --version | head -1
+
+# Fun
+fortune -v
+cowsay "It's working"
+
+# GNU Utilities
+gettext --version | head -1
+command -v find
+command -v getopt
+command -v sed
+
+# Networking / Internet Utilities
+wget --version | head -1
+sshuttle --version
+nmap --version | head -1
+
+# Utils
+
+# Compact files
+xz --version
+
+# Password Manager CLI
+lpass --version
+op --version
